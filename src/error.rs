461f186ba4399ad0f43940e66f1e48b1f04069f5
Praveen Perera <praveen@avencera.com>
2024-05-12 15:13:55 -0500

    Match public error names with internal names

--- src/error.rs
+++ src/error.rs
@@ -5,7 +5,7 @@ use crate::{decode, encode, header, join, split};
 pub type SplitError = split::SplitError;
 
 pub type JoinError = join::JoinError;
-pub type HeaderError = header::HeaderParseError;
+pub type HeaderParseError = header::HeaderParseError;
 
-pub type EncodingError = encode::EncodeError;
+pub type EncodeError = encode::EncodeError;
 pub type DecodeError = decode::DecodeError;
